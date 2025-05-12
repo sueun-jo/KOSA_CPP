bdfe144ac587dfae53d8825c6737f43e6d2014d0
sueun-jo <sueun1201@gmail.com>
2025-05-12 14:41:23 +0900

    Stack Class Ex

--- Class/main.cpp
+++ Class/main.cpp
@@ -11,6 +11,12 @@ public:
     void RemoveAll();
     bool Push(int value);
     bool Pop(int& value);
+
+    //접근자 함수
+    int GetSize();
+    int GetTop();
+    bool SetSize(int size);
+    bool GetData(int index, int& data);
 };
 
 void Stack::Initialize(int size){
@@ -39,6 +45,30 @@ bool Stack::Pop(int &value){
     return true;
 }
 
+int Stack::GetSize(){
+    return m_size;
+}
+
+int Stack::GetTop(){
+    return m_top;
+}
+
+bool Stack::SetSize(int size){
+    if(size < m_size) return false;
+    int* tmp = m_buffer;
+    m_size = size;
+    m_buffer = new int[m_size];
+    memcpy(m_buffer, tmp, sizeof(int)*(m_top)+1);
+    delete [] tmp;
+    return true;
+}
+
+bool Stack::GetData(int index, int& data){
+    if (index<0 || index>m_top) return false;
+    data = m_buffer[index];
+    return true;
+}
+
 
 int main()
 {
@@ -47,5 +77,6 @@ int main()
     int data;
     s1.Pop(data);
     s1.Push(123);
+
     return 0;
 }

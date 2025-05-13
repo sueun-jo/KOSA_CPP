18c87b4a4e107d230d2247e3bc5e40a282bb03d9
sueun-jo <sueun1201@gmail.com>
2025-05-13 16:24:08 +0900

    L value, R value && 연산자 예제

--- LRValueReference/main.cpp
+++ LRValueReference/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 #define ON 1
@@ -27,7 +28,7 @@ int main()
 }
 #endif
 
-#if ON
+#if OFF
 
 template <class T>{
 swap1(T& a, T &b){
@@ -44,5 +45,64 @@ swap2(T& a, T &b){
     a = move(b); //a를 L-Value에서 R-Value 참조 타입으로 캐스팅
     b = move(tmp);  //std::move(b)는 b를 R-Value처럼 취급하게 함
 }
+
+#endif
+
+#if OFF
+
+class MyClass
+{
+public:
+    MyClass(){} //생성자
+    MyClass(string str) : m_str(str){}
+
+    void print(){
+        cout << m_str << endl;
+    }
+
+private:
+    string m_str;
+};
+
+
+int main(){
+    MyClass A("aaa");
+    MyClass B;
+    MyClass C;
+
+    B=A;
+    C = move(A); //std::move;
+
+    A.print();
+    B.print();
+    C.print();
+
+    return 0;
+
+}
+#endif
+
+#if ON
+
+int main(){
+    string name1 = "allen";
+    cout << "name1: " << name1 << endl;
+
+    string name2 = move(name1);
+    cout << "After, name2 = move(name)" <<endl;
+    cout << "-> name1: " << name1 << endl;
+    cout << "-> name2: " << name2 << endl << endl;
+
+    //int형 데이터는 객체가 아니기 때문에 move해봤자 의미없음
+    //move()는 l-value를 r-value화 하는 거기 때문에 이미 r-value를 r-value해봤자 동작 안함
+    int zipcode1 = 90031;
+    cout << "zipcode1: " << zipcode1 << endl;
+    int zipcode2 = move(zipcode1);
+    cout << "After, zipcode2 = move(zipcode1)" << endl;
+    cout << "-> zipcode1: " << zipcode1 << endl;
+    cout << "-> zipcode2: " << zipcode2 << endl;
+
+}
+
 
 #endif

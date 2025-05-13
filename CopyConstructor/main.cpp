3645ae277e347d3e6725e5cc546f5f4641f0d301
sueun-jo <sueun1201@gmail.com>
2025-05-13 10:50:48 +0900

    복사생성자 예제

---
+++ CopyConstructor/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+#define ON 1
+#define OFF 0
+
+#if OFF
+class SoSimple{
+private:
+    int num1;
+    int num2;
+
+public:
+    SoSimple(int n1, int n2) : num1(n1), num2(n2)
+    {
+        //empty
+    }
+
+    //레퍼런스 변수로 객체 자체를 인자로 받기
+    SoSimple(SoSimple &copy) : num1(copy.num1), num2(copy.num2)
+    {
+        cout << "Called SoSimple(SoSimple &copy)" << endl;
+    }
+    void ShowSimpleData()
+    {
+        cout << num1 << endl;
+        cout << num2 << endl;
+    }
+
+};
+int main()
+{
+    SoSimple sim1(15, 30);
+    cout << "생성 및 초기화 직전" << endl;
+
+    SoSimple sim2 = sim1;
+    cout << "생성 및 초기화 직후" << endl;
+    sim2.ShowSimpleData();
+    return 0;
+}
+#endif
+
+#if ON
+
+class SoSimple{
+private:
+    int num1;
+
+
+public:
+    SoSimple(int n1) : num1(n1)
+    {
+        //empty
+    }
+
+    //레퍼런스 변수로 객체 자체를 인자로 받기
+    SoSimple(const SoSimple &copy) : num1(copy.num1)
+    {
+        cout << "Called SoSimple(const SoSimple &copy)" << endl;
+    }
+    void ShowData()
+    {
+        cout << num1 << endl;
+    }
+
+};
+
+void SimpleFuncObj(SoSimple ob){
+    ob.ShowData();
+}
+
+int main()
+{
+    SoSimple obj(7);
+    cout << "SimpleFuncObj 함수 호출 전" << endl;
+    SimpleFuncObj(obj);
+    cout << "SimpleFuncObj 함수 호출 후" << endl;
+
+    return 0;
+}
+
+#endif

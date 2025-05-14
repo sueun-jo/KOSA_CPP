79718c8f24089e61af0f5e035f6b86d121f36f7e
sueun-jo <sueun1201@gmail.com>
2025-05-14 17:13:31 +0900

    디폴트 대입연산자의 오버라이딩

--- TimeClass/main.cpp
+++ TimeClass/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+#define ON 1
+#define OFF 0
+#if OFF
 class Sueun{
 private:
     int age;
@@ -43,9 +46,60 @@ int main(void){
     sueun1.introduce();
     sueun2.introduce();
     sueun3.introduce();
+
     return 0;
 }
 
 void Sueun::introduce(){
     cout << "age: " << age << endl;
 }
+
+#endif
+
+#if ON
+
+class Person{
+private:
+    char * name;
+    int age;
+
+public:
+    Person(const char * myname, int myage){
+        int len = strlen(myname)+1;
+        name = new char[len];
+        strcpy(name, myname);
+        age = myage;
+    }
+
+    void ShowPersonInfo() const
+    {
+        cout << "이름: " << name << endl;
+        cout << "나이: " << age << endl;
+    }
+
+    ~Person(){
+        delete []name;
+        cout << "소멸자" << endl;
+    }
+
+    //해결책이 되는 대입연산자 오버라이딩
+    Person & operator = (const Person& ref){
+        delete []name;
+        int len = strlen(ref.name)+1;
+        name = new char[len];
+        strcpy(name, ref.name);
+        age = ref.age;
+        return *this;
+    }
+};
+
+int main (void){
+    Person man1("Jo Sueun", 26);
+    Person man2("Yoo Minho", 33);
+
+    man2 = man1;
+    man1.ShowPersonInfo();
+    man2.ShowPersonInfo();
+    return 0;
+}
+#endif

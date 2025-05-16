4da7193193bb28af434dd27469313d2d7436d433
sueun-jo <sueun1201@gmail.com>
2025-05-16 10:23:58 +0900

    가상 상속 예제 추가

--- multiInherit/main.cpp
+++ multiInherit/main.cpp
@@ -33,7 +33,7 @@ int main()
 }
 #endif
 
-#if ON /* 다중 상속 예제2  */
+#if OFF /* 다중 상속 예제2  */
 #include <iostream>
 using namespace std;
 
@@ -93,6 +93,106 @@ public:
 int main(void){
     Child child;
     child.write();
+    return 0;
+}
+#endif
+
+#if OFF
+#include <iostream>
+using namespace std;
+
+class BaseOne{
+public:
+    void SimpleFunc(){ cout << "BaseOne" << endl;}
+};
+
+class BaseTwo{
+public:
+    void SimpleFunc() {cout << "BaseTwo" << endl;}
+};
+
+class MultiDerived : public BaseOne, protected BaseTwo{
+public:
+    void ComplexFunc(){
+        BaseOne::SimpleFunc();
+        BaseTwo::SimpleFunc();
+    }
+};
+
+
+int main()
+{
+    MultiDerived mdr;
+    mdr.ComplexFunc();
+
+    return 0;
+}
+#endif
+
+#if ON /* 가상 상속 예제 */
+#include <iostream>
+using namespace std;
+
+class Person{
+public:
+    Person(){
+        cout << "Person::Person() 생성자" << endl;
+    }
+    ~Person(){
+        cout << "Person::Person() 소멸자" << endl;
+    }
+    virtual void write()=0; //순수 가상함수, Person을 상속받으면 파생클래스는 write함수 무조건 구현해야함
+    void sleep(){
+        cout << "잔다" << endl;
+    }
+};
+
+class Soo : virtual public Person {
+public:
+    Soo() : Person() {
+        cout << "Soo::Soo() 생성자" << endl;
+    }
+    ~Soo(){
+        cout << "Soo::~Soo() 소멸자" << endl;
+    }
+
+    void write(){
+        cout << "책을 잘 쓴다" << endl;
+    }
+};
+
+class Hee : virtual public Person {
+public:
+    Hee() : Person() {
+        cout << "Hee::Hee() 생성자" << endl;
+    }
+    ~Hee() {
+        cout << "Hee::~Hee() 소멸자" << endl;
+    }
+    void write(){
+        cout << "책을 못 쓴다" << endl;
+    }
+};
+
+class Child : public Soo, public Hee{
+public:
+    Child() : Soo(), Hee(){
+        cout << "Child::Child() 생성자" << endl;
+
+    }
+    ~Child() {
+        cout << "Child::~Child() 소멸자" << endl;
+    }
+    void write (){
+        //write();
+        Soo::write();
+    }
+};
+
+int main(void){
+    Child child;
+    child.write();
+    child.sleep();
     return 0;
 }
 #endif

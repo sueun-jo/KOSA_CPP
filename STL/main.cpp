2e473a6696da3280d8ba2488f1105f61af0647fb
sueun-jo <sueun1201@gmail.com>
2025-05-19 11:41:45 +0900

    string 정렬

--- STL/main.cpp
+++ STL/main.cpp
@@ -220,3 +220,37 @@ int main(){
 }
 
 #endif
+
+#if ON
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    int arr[10] = {97, 44, 23, 55, 61, 12, 3, 75, 27, 84};
+    cout << "정렬된 배열: ";
+
+    sort(arr, arr+10);
+    for (int i=0; i<10; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    vector <string> strings;
+    string s;
+    cout << "문자열을 입력하세요 : ";
+    while(cin >> s)
+        strings.push_back(s);
+
+    sort(strings.begin(), strings.end());
+    cout << "정렬된 문자열: ";
+    for (vector<string>::iterator it= strings.begin(); it != strings.end(); it++)
+        cout << *it << " ";
+    cout << endl;
+
+    return 0;
+}
+
+
+
+#endif

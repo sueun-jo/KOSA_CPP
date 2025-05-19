329eb799fbb35293691bd237181fad9ead1f9537
sueun-jo <sueun1201@gmail.com>
2025-05-19 11:12:29 +0900

    unordered map

--- STL/main.cpp
+++ STL/main.cpp
@@ -189,3 +189,34 @@ int main(void){
     return 0;
 }
 #endif
+
+#if OFF /* 비순차 연관 컨테이너 예제 - unordered map */
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    unordered_map<string, int> um;
+    um.insert({"A", 100});
+    um.insert(make_pair("B", 200));
+
+    um.insert({
+        {"C", 1000},
+        {"D", 2000}
+    });
+
+    for (auto i = um.begin(); i != um.end(); i++)
+        cout << "[" << i->first << " " << i->second << "]";
+    cout << endl;
+
+    cout <<"find B" << endl;
+    auto bc = um.bucket("B");
+
+    for (auto i = um.begin(bc); i != um.end(bc); i++)
+        cout << i->first << ": " << i->second;
+    cout << endl;
+
+    return 0;
+}
+
+#endif
